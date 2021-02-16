d8a2f3c41e610032761afb8e7b622b855fc2fdae
Ksenia <ksu0593@gmail.com>
2021-02-16 17:43:16 +0200

    16.2

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function externalLinks() {
-  for (
-    var c = document.getElementsByTagName('a'), a = 0;
-    a < c.length;
-    a++
-  ) {
-    var b = c[a];
-    b.getAttribute('href') &&
-      b.hostname !== location.hostname &&
-      (b.target = '_blank');
-  }
+function externalLinks()
+{
+    for (
+        var c = document.getElementsByTagName('a'), a = 0;
+        a < c.length;
+        a++
+    ) {
+        var b = c[ a ];
+        b.getAttribute('href') &&
+            b.hostname !== location.hostname &&
+            (b.target = '_blank');
+    }
 }
 externalLinks();
